eb17d02145c4d04b254a5820bdc3d1c4a99c129b
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-05-10 09:04:59 -0700

    order collection create and api also create

--- index.js
+++ index.js
@@ -24,7 +24,34 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const foodsCollection = client.db("foodsDB").collection("foods");
-
+    const orderCollections = client.db("foodsDB").collection("purchase");
+    //  customer buy data
+    app.post("/food/:id", async (req, res) => {
+      try {
+          const id = req.params.id;
+          const newOrder = req.body;
+        // update the food quantity
+        const foodQuery = {_id:new ObjectId(id)};
+        const food = await foodsCollection.findOne(foodQuery);
+        const updatedFoodQuantity = food.quantity - newOrder.quantity;
+        const updatedPurchaseQuantity = food.
+        purchaseQuantity+newOrder.quantity;
+        await foodsCollection.updateOne(foodQuery,
+          {
+            $set:
+            {
+              quantity:updatedFoodQuantity,
+              purchaseQuantity:updatedPurchaseQuantity
+         }})
+        
+          newOrder._id = id;
+          const result = await orderCollections.insertOne(newOrder);
+          res.send(result); 
+      } catch (error) {
+          console.error(error);
+          res.status(500).json({ error: 'Internal Server Error' });
+      }
+  });
     app.get("/food",async(req,res)=>{
       const cursor = foodsCollection.find();
       const result = await cursor.toArray();
@@ -41,6 +68,7 @@ async function run() {
         console.log(error);
       }
     })
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
