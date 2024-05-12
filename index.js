aa31b2b416d87d7fafc3acd51268f5fb31a70e25
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-05-12 11:27:53 -0700

    jwt install and cookie parser

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const cors = require("cors");
 require("dotenv").config();
+const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken');
 const app = express()
 const port = process.env.PORT||5000;
 // middleware
-app.use(cors());
+app.use(cors({
+  origin:[
+    'http://localhost:5173/'
+  ],
+  credentials:true,
+}));
 app.use(express.json());
-
+app.use(cookieParser());
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ipsrkdy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
